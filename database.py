437b1e8c8d06611a71cd104d774feef44a46f706
ChrisHanshia16 <hanshiachristo@gmail.com>
2023-09-29 10:07:27 +0530

    database updated

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://kujgzuuz:5XzWRlq7pzKX3Wq5hhPvHHoxgPu7xp-n@rajje.db.elephantsql.com/kujgzuuz'
+SQLALCHEMY_DATABASE_URL = 'postgres://bjjmrnvm:fVdhCRC9KOLyxKYL7e5R_d-aTMxvixWy@peanut.db.elephantsql.com/bjjmrnvm'
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
